5ea07c09c246887bf1391aece6e4d39787be1a7a
Wang Jie <2551434573@qq.com>
2022-08-09 16:24:40 +0800

    通过注解配置和包自动扫描的方式完成Bean对象的注册

---
+++ src/main/java/com/cafebabe/springframework/context/annotation/ClassPathScanningCandidateComponentProvider.java
@@ -0,0 +1,24 @@
+package com.cafebabe.springframework.context.annotation;
+
+import cn.hutool.core.util.ClassUtil;
+import com.cafebabe.springframework.beans.factory.config.BeanDefinition;
+import com.cafebabe.springframework.stereotype.Component;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+/**
+ * A component provider that scans the classpath from a base package. It then
+ * applies exclude and include filters to the resulting classes to find candidates.
+ * <p>
+ */
+public class ClassPathScanningCandidateComponentProvider {
+    public Set<BeanDefinition> findCandidateComponents(String basePackage) {
+        Set<BeanDefinition> candidates = new LinkedHashSet<>();
+        Set<Class<?>> classes = ClassUtil.scanPackageByAnnotation(basePackage, Component.class);
+        for (Class<?> clazz : classes) {
+            candidates.add(new BeanDefinition(clazz));
+        }
+        return candidates;
+    }
+}
